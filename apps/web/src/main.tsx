c7d5a9783e9e3ae75d0313b247f731485e5b86c6
Åsmund Røst Wien <asmundwien@gmail.com>
2026-09-26 15:48:35 +0200

    Separate reference color stylesheet

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import { RoadmapProvider } from './store/roadmap-provider'
+import './reference-colors.css'
 import './index.css'
 
 const root = document.getElementById('root')
